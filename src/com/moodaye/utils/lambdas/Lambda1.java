af93127f62460821badeee70d078a9f663219238
Mood Aye <rajivkewal@aol.com>
2020-01-01 10:55:11 -0500

    New Lambda to print unique n ints that sum to zero.

--- src/com/moodaye/utils/lambdas/Lambda1.java
+++ src/com/moodaye/utils/lambdas/Lambda1.java
@@ -5,9 +5,20 @@ import java.util.stream.IntStream;
 public class Lambda1 {
 
 	/** returning a stream of char integers from a String */
-	/*  chars is derived from CharSequence interface */
-	public static IntStream streamCharIntFromString(String s){
+	/* chars is derived from CharSequence interface */
+	public static IntStream streamCharIntFromString(String s) {
 		return s.chars();
 	}
 
+	public static void findNUniqueIntsSumZero(int n) {
+		IntStream.rangeClosed(0, n / 2).forEach(x -> {
+			if (x == 0) {
+				if (n % 2 == 1) {
+					System.out.print(0 + " ");
+				}
+			} else {
+				System.out.print(x + " " + -x + " ");
+			}
+		});
+	}
 }
